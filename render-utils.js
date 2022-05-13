b4b518aad1043b20cb12c2209a65be992433c0f5
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-12 17:09:26 -0700

    pictures uploading

--- render-utils.js
+++ render-utils.js
@@ -1,17 +1,19 @@
 export function renderStadiumLi(stadium) {
-    const div = document.getElementById('div');
+    const div = document.createElement('div');
     const li = document.createElement('li');
     const a = document.createElement('a');
-    const img = document.getElementById('img');
+    const img = document.createElement('img');
 
-    img.src = `./assets/${stadium.name}`;
-    a.href = `/stadiums-detail/?id=${stadium.id}`;
+    div.classList.add('stadium-detail-container');
+
+    img.src = `./assets/${stadium.image}.jpg`;
+    a.href = `./stadiums-detail/?id=${stadium.id}`;
     a.textContent = stadium.name;
-    div.append(img);
 
-    li.append(a);
 
-    return li;
+    a.append(img, li);
+    div.append(a);
+    return div;
 }
 
 
@@ -20,6 +22,10 @@ export function renderStadiumDetail(stadium) {
     const div = document.createElement('div');
     const img = document.createElement('img');
     const placeEl = document.createElement('p');
+    const capacityEl = document.createElement('p');
+
+    capacityEl.textContent = stadium.capacity;
+    capacityEl.classList.add('capacity');
 
     placeEl.textContent = stadium.place;
     placeEl.classList.add('place');
@@ -28,8 +34,9 @@ export function renderStadiumDetail(stadium) {
     div.classList.add('stadium-detail');
 
     img.src = `../assets/${stadium.name}.jpg`;
+    
+    div.append(capacityEl, img, placeEl);
 
-
-
+    return div;
     
 }
